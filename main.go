d87c953982ae4906cd3ceeae29e4fe84a4abb1e1
nirasan <niranirao@gmail.com>
2016-10-21 19:01:13 +0900

    impl CRUD

--- main.go
+++ main.go
@@ -12,8 +12,10 @@ import (
 
 func CreateHandler() http.Handler {
 	r := mux.NewRouter()
-	r.HandleFunc("/entry/put", EntryPutHandler)
-	r.HandleFunc("/entry/get", EntryGetHandler)
+	r.HandleFunc("/entry/create", EntryCreateHandler)
+	r.HandleFunc("/entry/show", EntryShowHandler)
+	r.HandleFunc("/entry/update", EntryUpdateHandler)
+	r.HandleFunc("/entry/delete", EntryDeleteHandler)
 	return r
 }
 
@@ -23,7 +25,7 @@ type Entry struct {
 	CreatedTime time.Time
 }
 
-func EntryPutHandler(w http.ResponseWriter, r *http.Request) {
+func EntryCreateHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
 	key := datastore.NewIncompleteKey(ctx, "Entry", nil)
 	e := &Entry{
@@ -34,11 +36,11 @@ func EntryPutHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	} else {
-		fmt.Fprintln(w, "put entry: IntID=", key.IntID())
+		fmt.Fprintln(w, "entry created: IntID=", key.IntID())
 	}
 }
 
-func EntryGetHandler(w http.ResponseWriter, r *http.Request) {
+func EntryShowHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
 	id, _ := strconv.ParseInt(r.FormValue("id"), 10, 64)
 	key := datastore.NewKey(ctx, "Entry", "", id, nil)
@@ -47,6 +49,37 @@ func EntryGetHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	} else {
-		fmt.Fprintln(w, "title: ", e.Title, " body: ", e.Body)
+		fmt.Fprintln(w, "entry show: title=", e.Title, " body=", e.Body)
+	}
+}
+
+func EntryUpdateHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	id, _ := strconv.ParseInt(r.FormValue("id"), 10, 64)
+	key := datastore.NewKey(ctx, "Entry", "", id, nil)
+	var e Entry
+	if err := datastore.Get(ctx, key, &e); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	e.Title = r.FormValue("t")
+	e.Body = r.FormValue("b")
+	if key, err := datastore.Put(ctx, key, &e); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	} else {
+		fmt.Fprintln(w, "entyr updated: IntID=", key.IntID())
+	}
+}
+
+func EntryDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	id, _ := strconv.ParseInt(r.FormValue("id"), 10, 64)
+	key := datastore.NewKey(ctx, "Entry", "", id, nil)
+	if err := datastore.Delete(ctx, key); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	} else {
+		fmt.Fprintln(w, "entyr deleted: IntID=", key.IntID())
 	}
 }
